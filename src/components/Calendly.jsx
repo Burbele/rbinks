88839c37a90c5945f8ec37ecf6f9cc1b4d754c7c
Paulius Burba <pauliusburba7@gmail.com>
2024-05-09 15:20:11 +0200

    preliminary design

--- src/components/Calendly.jsx
+++ src/components/Calendly.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Calendly = () => {
   return (
-    <div className="h-[1000px] md:h-[1000px] lg:h-[1000px] relative mt-[120px] lg:mt-[150px]">
+    <div className="bg-[#f9f9f9] h-[1000px] md:h-[1000px] lg:h-[1000px] relative  ">
       <iframe
         src="https://calendly.com/rebecabianca1211/tattoo-appointment"
         className="w-full h-full"
--- src/pages/AppointmentsPage.jsx
+++ src/pages/AppointmentsPage.jsx
@@ -1,11 +1,25 @@
 import React from 'react';
 import Calendly from '../components/Calendly';
-
+import { motion } from 'framer-motion';
+import { fadeIn } from '../variants';
 
 const AppointmentsPage = () => {
   return (
-    <section className='min-h-[40vh] lg:h-[948px] relative mt-[120px] lg:mt-[150px]'>
-     <Calendly />
+    <section className='bg-[#f9f9f9] relative mt-[120px] lg:mt-[150px] pt-16 items-center'>
+      <div className='container mx-auto'>
+        <motion.h2
+          variants={fadeIn('right')}
+          initial='hidden'
+          whileInView={'show'}
+          viewport={{ once: false, amount: 0.6 }}
+          className='h2 max-w-full lg:mb-10 text-left lg:text-center'>
+          Book Your Tattoo Session
+        </motion.h2>
+        <p className='text-left lg:text-center mx-auto mb-6 lg:w-[1000px]'>
+          Below you can see my availability. A full day session costs 1000 kr, with a 500 kr deposit required. This is a preliminary booking; I will contact you for design references, tattoo ideas, and payment instructions after you book.
+        </p>
+      </div>
+      <Calendly />
     </section>
   );
 };
