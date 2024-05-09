4658e760a72831105182a725da6de8745e05c3df
Paulius Burba <pauliusburba7@gmail.com>
2024-05-10 00:04:02 +0200

    Burger Menu Fix

--- src/components/NavMobile.jsx
+++ src/components/NavMobile.jsx
@@ -3,34 +3,49 @@ import { useTranslation } from 'react-i18next';
 import { Link } from 'react-router-dom';
 import LanguageSelector from './LanguageSelector';
 
-const NavMobile = () => {
-    const { t } = useTranslation();
-    const { navhome, navabout, navgallery, navaftercare, navappointments } = t("navData");
+const NavMobile = ({ setNavMobile }) => {
+  const { t } = useTranslation();
+  const { navhome, navabout, navgallery, navaftercare, navappointments } = t("navData");
 
-    return (
-        <nav className='w-full h-full flex flex-col justify-evenly overflow-hidden'>
-            <ul className='flex flex-col justify-center items-center gap-y-6 py-6 mb-8'>
-                <li>
-                    <Link to="/" className='text-2xl font-primary uppercase' onClick={() => window.scrollTo({ top: 0, behavior: 'instant' })}>{navhome}</Link>
-                </li>
-                <li>
-                    <Link to="/about" className='text-2xl font-primary uppercase' onClick={() => window.scrollTo({ top: 0, behavior: 'instant' })}>{navabout}</Link>
-                </li>
-                <li>
-                    <Link to="/gallery" className='text-2xl font-primary uppercase' onClick={() => window.scrollTo({ top: 0, behavior: 'instant' })}>{navgallery}</Link>
-                </li>
-                <li>
-                    <Link to="/aftercare" className='text-2xl font-primary uppercase' onClick={() => window.scrollTo({ top: 0, behavior: 'instant' })}>{navaftercare}</Link>
-                </li>
-                <li>
-                    <Link to="/appointments" className='text-2xl font-primary uppercase' onClick={() => window.scrollTo({ top: 0, behavior: 'instant' })}>{navappointments}</Link>
-                </li>
-            </ul>
-            <div className='text-2xl'>
-                <LanguageSelector />
-            </div>
-        </nav>
-    );
+  const handleLinkClick = () => {
+    setNavMobile(false);
+    window.scrollTo({ top: 0, behavior: 'instant' });
+  };
+
+  return (
+    <nav className='w-full h-full flex flex-col justify-evenly overflow-hidden'>
+      <ul className='flex flex-col justify-center items-center gap-y-6 py-6 mb-8'>
+        <li>
+          <Link to="/" className='text-2xl font-primary uppercase' onClick={handleLinkClick}>
+            {navhome}
+          </Link>
+        </li>
+        <li>
+          <Link to="/about" className='text-2xl font-primary uppercase' onClick={handleLinkClick}>
+            {navabout}
+          </Link>
+        </li>
+        <li>
+          <Link to="/gallery" className='text-2xl font-primary uppercase' onClick={handleLinkClick}>
+            {navgallery}
+          </Link>
+        </li>
+        <li>
+          <Link to="/aftercare" className='text-2xl font-primary uppercase' onClick={handleLinkClick}>
+            {navaftercare}
+          </Link>
+        </li>
+        <li>
+          <Link to="/appointments" className='text-2xl font-primary uppercase' onClick={handleLinkClick}>
+            {navappointments}
+          </Link>
+        </li>
+      </ul>
+      <div className='text-2xl'>
+        <LanguageSelector />
+      </div>
+    </nav>
+  );
 };
 
 export default NavMobile;
