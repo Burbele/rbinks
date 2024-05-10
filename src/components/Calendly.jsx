b3a9728833e295b552a06e5dd8f95b0f5746eea6
Paulius Burba <pauliusburba7@gmail.com>
2024-05-10 11:23:02 +0200

    Calendly Fix

--- src/components/Calendly.jsx
+++ src/components/Calendly.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 
 const Calendly = () => {
   return (
-    <div className="calendly-wrapper h-screen relative overflow-hidden">
+    <div className="calendly-wrapper h-screen relative overflow-hidden mb-[2rem]">
       <iframe
         src="https://calendly.com/rebecabianca1211/tattoo-appointment"
-        className="calendly-iframe w-full h-full"
+        className="calendly-iframe w-full h-full lg:min-h-[1200px] lg:min-w-[1000px] md:min-h-[800px] md:min-w-[800px] xl:min-h-[1200px] xl:min-w-[1000px]"
         frameBorder="0"
         title="Calendly Appointment"
       ></iframe>
--- src/pages/AppointmentsPage.jsx
+++ src/pages/AppointmentsPage.jsx
@@ -9,7 +9,7 @@ const AppointmentsPage = () => {
   const { t } = useTranslation();
 
   return (
-    <section className='bg-[#f9f9f9] relative mt-[120px] lg:mt-[150px] pt-16 flex flex-col items-center h-screen overflow-hidden'>
+    <section className='bg-[#f9f9f9] relative mt-[120px] lg:mt-[150px] pt-16 flex flex-col items-center h-full'>
       <Helmet>
         <title>Book a Tattoo Session | Rbinks - Tattoo Appointments</title>
         <meta name="description" content="Book a full day tattoo session with Rebeca at Epic Ink Studio. Work on large projects or multiple smaller tattoos. Secure your booking with a 500 kr deposit." />
