c3d61fb7ecdec05ed5d9a721a49ea9ed96758fef
Paulius Burba <pauliusburba7@gmail.com>
2024-05-14 21:58:44 +0200

    text-grey

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -31,10 +31,10 @@ const AboutPage = () => {
       <div className='container mx-auto grid w-full grid-cols-1 md:grid-cols-7 gap-16 justify-between'>
         <div className='col-span-1 md:col-span-4 flex flex-col items-start justify-start'>
           <h2 className='mb-4 text-lg font-bold uppercase text-dark/75'>{t('subtitle2')}</h2>
-          <p className='font-medium'>{t('aboutpage.about1')}</p>
-          <p className='my-4 font-medium'>{t('aboutpage.about2')}</p>
-          <p className='my-4 font-medium'>{t('aboutpage.about3')}</p>
-          <p className='font-medium'>{t('aboutpage.about4')}</p>
+          <p className='font-medium text-grey'>{t('aboutpage.about1')}</p>
+          <p className='my-4 font-medium text-grey'>{t('aboutpage.about2')}</p>
+          <p className='my-4 font-medium text-grey'>{t('aboutpage.about3')}</p>
+          <p className='font-medium text-grey'>{t('aboutpage.about4')}</p>
         </div>
         <div className='col-span-1 md:col-span-3 relative h-max rounded-2xl border-2 border-solid border-dark bg-light p-8'>
           <img src={aboutPicture} alt='' className='w-full h-auto rounded-2xl' />
