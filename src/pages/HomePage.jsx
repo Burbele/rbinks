53172cacf59a0e1462aed60d5684a4042745d791
Paulius Burba <pauliusburba7@gmail.com>
2024-05-14 22:10:18 +0200

    seo

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -11,7 +11,7 @@ const HomePage = () => {
       <Helmet>
         <title>Home | Rbinks - Tattoo Apprentice in Aarhus, Denmark</title>
         <meta name="description" content="Welcome to Rbinks' portfolio. Rebeca is a tattoo apprentice based in Aarhus, Denmark. Explore her unique and elegant tattoo designs and book a session at an affordable rate." />
-        <meta name="keywords" content="tattoo apprentice, affordable tattoos, Aarhus, Denmark, Rbinks, Rebeca, tattoo designs, book appointment, tattoo portfolio" />
+        <meta name="keywords" content="tattoo apprentice, affordable tattoos, Aarhus, Denmark, Rbinks, Rebeca, Rebeca Bianca Serban, tattoo designs, book appointment, tattoo portfolio" />
         <meta name="author" content="Rbinks - Rebeca" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Helmet>
