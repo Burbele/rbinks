84995fbe05f11ee4e08ec0f8391a2b0318439930
Paulius Burba <pauliusburba7@gmail.com>
2024-05-17 11:15:20 +0200

    Accordion - open/close

--- src/components/AftercareQ.jsx
+++ src/components/AftercareQ.jsx
@@ -8,21 +8,20 @@ function AftercareQ({ question, answer }) {
 
     return (
         <div className='shadow rounded border border-gray-100 border-t-0 bg-white'>
-            <div className='p-4 text-base xl:text-xl relative font-medium'>
+            <div 
+                className='p-4 text-base xl:text-xl relative font-medium cursor-pointer'
+                onClick={() => setExpand(!expand)}
+            >
                 <div className='w-5/6'>
                     {question}
                 </div>
-                <button
-                    aria-label='question-expander'
-                    className='text-base xl:text-xl absolute top-0 right-0 p-4 focus:outline-none'
-                    onClick={() => setExpand(!expand)}
-                >
+                <div className='text-base xl:text-xl absolute top-0 right-0 p-4'>
                     {expand ?
                         <FaChevronUp className='w-5' />
                         :
                         <FaChevronDown className='w-5' />
                     }
-                </button>
+                </div>
             </div>
             <div className={ansClass}>
                 {answer}
@@ -32,4 +31,3 @@ function AftercareQ({ question, answer }) {
 }
 
 export default AftercareQ;
-
